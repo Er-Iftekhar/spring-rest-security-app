da151466b3e503fb7d4fbcae77ea1de180ef3376
SyedIftekharuddin <syedy636@gmail.com>
2020-09-27 19:32:07 +0200

    Enabling the project to create a war file for deployment.

--- src/main/java/com/app/utb/springrestsecurityapp/SpringRestSecurityAppApplication.java
+++ src/main/java/com/app/utb/springrestsecurityapp/SpringRestSecurityAppApplication.java
@@ -16,5 +16,4 @@ public class SpringRestSecurityAppApplication extends SpringBootServletInitializ
     public static void main(String[] args) {
         SpringApplication.run(SpringRestSecurityAppApplication.class, args);
     }
-
 }
