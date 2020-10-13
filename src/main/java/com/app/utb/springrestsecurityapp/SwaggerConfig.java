95b6bfc9442d0c6e6b0d6eead5c6e48a2ace3827
SyedIftekharuddin <syedy636@gmail.com>
2020-10-13 19:35:36 +0200

    Uploading API documentation information.

--- src/main/java/com/app/utb/springrestsecurityapp/SwaggerConfig.java
+++ src/main/java/com/app/utb/springrestsecurityapp/SwaggerConfig.java
@@ -9,20 +9,50 @@ import org.springframework.plugin.core.SimplePluginRegistry;
 import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
+    Contact contact = new Contact(
+            "Iftekharuddin Syed",
+            "http://www.swagger.org",
+            "syedy636@gmail.com"
+    );
+
+
+    List<VendorExtension> vendorExtensions = new ArrayList<>();
+
+    ApiInfo apiInfo = new ApiInfo(
+      "Photo app RESTful Web Service documentation",
+      "This pages documents Photo app RESTful Web Service endpoints",
+      "1.0",
+      "http://www.swagger.org",
+      contact,
+      "Apache 2.0",
+      "http://www.apache.org/licenses/LICENSE-2.0",
+      vendorExtensions
+    );
+
     @Bean
     public Docket apiDocket(){
         Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .protocols(new HashSet<>(Arrays.asList(
+                        "HTTP",
+                        "HTTPS"
+                )))
+                .apiInfo(apiInfo)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.app.utb.springrestsecurityapp"))
                 .paths(PathSelectors.any())
