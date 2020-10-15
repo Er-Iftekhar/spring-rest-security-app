fb70b7bc90bfa43cadc7d061412b2ab228c7f802
SyedIftekharuddin <syedy636@gmail.com>
2020-10-15 09:21:38 +0200

    Created User with admin role.

--- src/main/java/com/app/utb/springrestsecurityapp/InitialUsersSetup.java
+++ src/main/java/com/app/utb/springrestsecurityapp/InitialUsersSetup.java
@@ -3,14 +3,17 @@ package com.app.utb.springrestsecurityapp;
 
 import com.app.utb.springrestsecurityapp.entity.AuthorityEntity;
 import com.app.utb.springrestsecurityapp.entity.RoleEntity;
+import com.app.utb.springrestsecurityapp.entity.UserEntity;
 import com.app.utb.springrestsecurityapp.repositories.AuthorityRepository;
 import com.app.utb.springrestsecurityapp.repositories.RoleRepository;
+import com.app.utb.springrestsecurityapp.repositories.UserRepository;
+import com.app.utb.springrestsecurityapp.utils.Utils;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -19,10 +22,16 @@ public class InitialUsersSetup {
 
     private final AuthorityRepository authorityRepository;
     private final RoleRepository roleRepository;
+    private final Utils utils;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 
-    public InitialUsersSetup(AuthorityRepository authorityRepository, RoleRepository roleRepository) {
+    public InitialUsersSetup(AuthorityRepository authorityRepository, RoleRepository roleRepository, Utils utils, PasswordEncoder passwordEncoder, UserRepository userRepository) {
         this.authorityRepository = authorityRepository;
         this.roleRepository = roleRepository;
+        this.utils = utils;
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
     }
 
     @EventListener
@@ -33,8 +42,21 @@ public class InitialUsersSetup {
         AuthorityEntity writeAuthority = createAuthority("WRITE_AUTHORITY");
         AuthorityEntity deleteAuthority = createAuthority("DELETE_AUTHORITY");
 
-        createRole("ROLE_USER", Arrays.asList(readAuthority, writeAuthority));
-        createRole("ROLE_ADMIN", Arrays.asList(readAuthority, writeAuthority, deleteAuthority));
+        RoleEntity userRole = createRole("ROLE_USER", Arrays.asList(readAuthority, writeAuthority));
+
+        RoleEntity roleAdmin = createRole("ROLE_ADMIN", Arrays.asList(readAuthority, writeAuthority, deleteAuthority));
+
+        if(roleAdmin !=null){
+            UserEntity userEntity = new UserEntity();
+            userEntity.setFirstName("admin");
+            userEntity.setLastName("admin");
+            userEntity.setEmail("admin@gmail.com");
+            userEntity.setEmailVerificationStatus(true);
+            userEntity.setUserId(utils.generateUserId(30));
+            userEntity.setEncryptedPassword(passwordEncoder.encode("pass"));
+            userEntity.setRoles(Arrays.asList(roleAdmin));
+            userRepository.save(userEntity);
+        }
 
     }
 
