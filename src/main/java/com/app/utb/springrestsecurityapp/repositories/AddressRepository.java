e5c6a6cb3cb2ab3ac263fc64d717a5ab07735dcf
SyedIftekharuddin <syedy636@gmail.com>
2020-10-17 14:20:08 +0200

    Enabling method level security and @Secured annotations.

--- src/main/java/com/app/utb/springrestsecurityapp/repositories/AddressRepository.java
+++ src/main/java/com/app/utb/springrestsecurityapp/repositories/AddressRepository.java
@@ -2,14 +2,16 @@ package com.app.utb.springrestsecurityapp.repositories;
 
 import com.app.utb.springrestsecurityapp.entity.AddressEntity;
 import com.app.utb.springrestsecurityapp.entity.UserEntity;
+import com.app.utb.springrestsecurityapp.service.UserService;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 
 @Repository
-public interface AddressRepository extends CrudRepository<AddressEntity, Long> {
+public interface AddressRepository  extends CrudRepository<AddressEntity, Long>  {
     List<AddressEntity> findAllByUserDetails(UserEntity userEntity);
 
     AddressEntity findByAddressId(String addressId);
--- src/main/java/com/app/utb/springrestsecurityapp/security/SecurityConfigurations.java
+++ src/main/java/com/app/utb/springrestsecurityapp/security/SecurityConfigurations.java
@@ -5,6 +5,7 @@ import com.app.utb.springrestsecurityapp.repositories.UserRepository;
 import com.app.utb.springrestsecurityapp.service.UserService;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableGlobalMethodSecurity(securedEnabled = true)
 @EnableWebSecurity
 public class SecurityConfigurations   extends WebSecurityConfigurerAdapter {
 
